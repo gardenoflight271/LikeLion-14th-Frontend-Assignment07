50187687443a09649a63353b158fda7c00c2e9a5
Lee Chaewon <hyr.chaewon@gmail.com>
2026-06-06 00:30:41 +0900

    제출2트

--- cwlee/src/components/Toast.jsx
+++ cwlee/src/components/Toast.jsx
@@ -1,5 +1,5 @@
 export const Toast = ({ message }) => {
     return (
-        <div className="fixed bottom-8 bg-black text-white">{message}</div>
+        <div className="fixed bottom-8 bg-slate-200/50 left-1/2 -translate-x-1/2 text-black p-2 rounded-2xl">{message}</div>
     )
 }
